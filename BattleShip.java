e61f4438efd41c985a7077acf451d65a70623bdc
shahnoormhabib <shahnoor_h@Yahoo.com>
2017-09-01 12:34:34 -0500

    Add files via upload

---
+++ BattleShip.java
@@ -0,0 +1,10 @@
+
+
+
+class BattleShip { 
+public static void main(String[] arguments){
+	   BattleShip_Modular myGame=new BattleShip_Modular();
+	   myGame.GameIsOn();
+	 //start the battle	 
+   }//main function ends
+}
